3fdcf0bcd98b3cafc92aeb635b75dbf77d417493
Klivens Ziu <klivens.ziu@epitech.eu>
2020-06-23 15:48:26 +0200

    mongodb link changed

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,7 @@
-exports.dbCFG = "mongodb://localhost:27017/codac";
+exports.dbCFG =
+  "mongodb+srv://klivens:klivens1epitech@codac-pff-mmnhy.gcp.mongodb.net/codac-pff?retryWrites=true&w=majority";
+//mongodb+srv://klivens:klivens1epitech@codac-pff-mmnhy.gcp.mongodb.net/codac-pff?retryWrites=true&w=majority
+//mongodb://localhost:27017/codac
 process.env.PORT = 4242;
 process.env.NODE_ENV = "development";
 exports.JWT_SECRET = "3ce@K8bfp:N4CHK}K3<K}y@e}47]N";
